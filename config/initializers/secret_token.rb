390fca7488d8c1256512af6e85d79619690ec586
Bence Erdős <bence.erdos@proinvento.hu>
2010-11-22 20:45:06 +0100

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProinventSite::Application.config.secret_token = 'b1c1134d35b44990ba33297a4d7757c788e2a4925fcad08d58b181162621b37bf84f62813aa8dea9d688b91e03f64d90eec90a08865f26a3803cf0fb2c93a4ed'
